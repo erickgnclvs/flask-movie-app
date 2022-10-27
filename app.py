2f51c80b8cc6def947319d66188b76232a1113ac
erickgnclvs <erickgoncalves5@hotmail.com>
2022-10-27 17:23:41 -0300

    Change navbar and add empty routes on app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -6,14 +7,45 @@ app = Flask(__name__)
 def index():
     return redirect('/home')
 
-@app.route('/home')
+@app.route('/home', methods=['POST', 'GET'])
 def home():
-    return render_template('index.html')
+    return render_template('about.html')
 
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    return render_template('sorry.html')
 
-#todo
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    return render_template('sorry.html')
 
-    
+@app.route('/favorites', methods=['POST', 'GET'])
+def favorites():
+    return render_template('sorry.html')
+
+@app.route('/add', methods=['POST', 'GET'])
+def add():
+    return render_template('sorry.html')
+
+@app.route('/delete', methods=['POST', 'GET'])
+def delete():
+    return render_template('sorry.html')
+
+@app.route('/changepassword', methods=['POST', 'GET'])
+def changepassword():
+    return render_template('sorry.html')
+
+@app.route('/logout', methods=['POST', 'GET'])
+def logout():
+    return render_template('sorry.html')
+
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    return render_template('sorry.html')
+
+@app.route('/forgotpassword', methods=['POST', 'GET'])
+def password():
+    return render_template('sorry.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
