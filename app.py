16b643d82809c9c7201d73c4ce4fb80170d3a22c
erickgnclvs <erickgoncalves5@hotmail.com>
2022-10-28 19:56:21 -0300

    Implement working register route

--- app.py
+++ app.py
@@ -16,13 +16,13 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Set database
+# Configure database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'fi2o42j3o4ij234o23jjj234jklh'
 db = SQLAlchemy(app)
 
-# Create database
+# Set database
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
@@ -59,86 +59,35 @@ def home():
 
 @app.route('/about', methods=['GET'])
 def about():
+    # This displays what the project is going to be and my Instagram and GitHub
     return render_template('about.html')
 
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
-    return render_template('register.html')
-#    # This wil register the user
-#
-#    # First do get method, to just display the register page
-#    if request.method == 'GET':
-#        return render_template('register.html')
-#
-#    # Then do post method, to submit users input
-#    else:
-#        username = request.form.get('username')
-#        password = request.form.get('password')
-#        confirmation = request.form.get('confirmation')
-#
-#        # check for problems
-#        if not username or not password or not confirmation:
-#            return render_template('error.html')
-#
-#        if password != confirmation:
-#            return render_template('error.html')
-#
-#        # submit input
-#        hash = generate_password_hash(password)
-#
-#        # check if username not in database
-#        #try:
-#            #session = 
-#        db.engine.execute('INSERT INTO users (username, hash) VALUES (?, ?)', username, hash).execution_options(autocommit=True)
-#
-#        #except:
-#        #    return "cant insert data in db" #render_template('error.html')
-#
-#
-#        #session['user_id'] = session
-#
-#        return redirect('/')
+    # This will register the user
+    # Thanks https://www.youtube.com/c/MrAmithsChannel for the help!
 
+    if request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+        userdata = Users(
+            username = username,
+            hash = password,
+        )
+
+        userdata.set_password(password)
+        db.session.add(userdata)
+        db.session.commit()
+        return redirect('/')
+
+    # This will render the registration form
+    else:
+        return render_template('register.html')
 
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-#    """Log user in"""
-#
-#    # Forget any user_id
-#    session.clear()
-#
-#    # User reached route via POST (as by submitting a form via POST)
-#    if request.method == "POST":
-#
-#        # Declare login variables
-#        username = request.form.get('username')
-#        password = request.form.get('password')
-#
-#        # Ensure username was submitted
-#        if not username:
-#            return 'username was not submited' #render_template('error.html')
-#
-#        # Ensure password was submitted
-#        elif not password:
-#            return 'password was not submited' #render_template('error.html')
-#
-#        # Query database for username
-#        userdata = db.engine.execute("SELECT * FROM users WHERE username = ?", username)
-#
-#        # Ensure username exists and password is correct
-#        if len(userdata) != 1 or not check_password_hash(userdata[0]["hash"], password):
-#            return 'username doesnt exist or password is incorrect' #render_template('error.html')
-#
-#        # Remember which user has logged in
-#        session["user_id"] = rows[0]["id"]
-#
-#        # Redirect user to home page
-#        return redirect("/")
-#
-#    # User reached route via GET (as by clicking a link or via redirect)
-#    else:
     return render_template("login.html")
 
 
